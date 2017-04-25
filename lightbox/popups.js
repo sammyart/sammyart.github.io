77a98c3182994a97beca4e3c49a09c214afc1c79
sammyart <jeffhales.arizona@gmail.com>
2017-04-25 14:00:34 -0700

    Update popups.js

--- lightbox/popups.js
+++ lightbox/popups.js
@@ -8,7 +8,65 @@ $(document).ready(function () {
       easing: 'ease-in-out'
     }
   });
-  $('#eoc-popup').magnificPopup({
+  $('#Starbucks').magnificPopup({
+    items: [
+      { src: 'img/Starbucks/WPS_Holiday Duo_slide_1080x1375.jpg' },
+      { src: 'img/Starbucks/blender wps_1080.jpg' },
+      { src: 'img/Starbucks/Frappy Hour_1080x1375.jpg' },
+      { src: 'img/Starbucks/Caramel Macchiato_WPSslide_1080x1375.jpg' },
+      { src: 'img/Starbucks/Mango Iced Tea_WPSslide_1080x1375.jpg' }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+  });
+  $('#City_of_Tempe').magnificPopup({
+    items: [
+      { src: 'img/Tiger/Tiger_main 1920x1080.jpg' },
+      { src: 'img/Tiger/black-tiger-t.jpg' }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+  });
+  $('#Einstein_Bros_Bagels').magnificPopup({
+    items: [
+      { src: 'img/EBB/EBB Tiramisu mob_1080.jpg' },
+      { src: 'img/EBB/EBB Tiramisu_venice_1080.jpg' },
+      { src: 'img/EBB/EBB Irish Cream_1080.jpg' },
+      { src: 'img/EBB/EBB Italian Soda_1080.jpg' }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+  });
+  $('#Samamama').magnificPopup({
+    items: [
+      { src: 'img/Samamama/Samamama_web_main1920.jpg' },
+      { src: 'img/Samamama/sama collections screen_1920x1080.jpg' },
+      { src: 'img/Samamama/Samamama package_1920x1080.jpg' }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+  });
+  $('#Wish-Bone').magnificPopup({
+    items: [
+      { src: 'img/Wish-Bone/italian bottle frame_1920.jpg' },
+      { src: 'img/Wish-Bone/WB couple_1920.jpg' },
+      { src: "img/Wish-Bone/worlds color frame_1920.jpg" },
+      { src: 'https://www.youtube.com/watch?v=uFXYwTafVqc?rel=0', type: 'iframe' }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+  });
+  $('#Eat_On_Campus').magnificPopup({
     items: [
       { src: 'img/EOC/Campus Eats_main 1080x1375.jpg' },
       { src: 'img/EOC/EOC Tree_1080.jpg' },
@@ -22,86 +80,7 @@ $(document).ready(function () {
     },
     type: 'image' // this is a default type
   });
-  $('#samamama-popup').magnificPopup({
-    items: [
-      { src: 'img/Samamama/Samamama_web_main1920.jpg' },
-      { src: 'img/Samamama/sama collections screen_1920x1080.jpg' },
-      { src: 'img/Samamama/Samamama package_1920x1080.jpg' }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-  $('#tiger-popup').magnificPopup({
-    items: [
-      { src: 'img/Tiger/Tiger_main 1920x1080.jpg' },
-      { src: 'img/Tiger/black-tiger-t.jpg' }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-  $('#ebb-popup').magnificPopup({
-    items: [
-      { src: 'img/EBB/EBB Tiramisu mob_1080.jpg' },
-      { src: 'img/EBB/EBB Tiramisu_venice_1080.jpg' },
-      { src: 'img/EBB/EBB Irish Cream_1080.jpg' },
-      { src: 'img/EBB/EBB Italian Soda_1080.jpg' }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-  $('#starbucks-popup').magnificPopup({
-    items: [
-      { src: 'img/Starbucks/WPS_Holiday Duo_slide_1080x1375.jpg' },
-      { src: 'img/Starbucks/blender wps_1080.jpg' },
-      { src: 'img/Starbucks/Frappy Hour_1080x1375.jpg' },
-      { src: 'img/Starbucks/Caramel Macchiato_WPSslide_1080x1375.jpg' },
-      { src: 'img/Starbucks/Mango Iced Tea_WPSslide_1080x1375.jpg' }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-  $('#farm2fork-popup').magnificPopup({
-    items: [
-      { src: '' },
-      { src: 'img/Farm2Fork/Farm 2 Fork Menu_menu.jpg' }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-  $('#juicecore-popup').magnificPopup({
-    items: [
-      { src: 'img/JuiceCore/JC main_1920x1080.jpg' },
-      { src: 'img/JuiceCore/Meetro spread_1920x1080.jpg' }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-  $('#wish-bone-popup').magnificPopup({
-    items: [
-      { src: 'img/Wish-Bone/italian bottle frame_1920.jpg' },
-      { src: 'img/Wish-Bone/WB couple_1920.jpg' },
-      { src: "img/Wish-Bone/worlds color frame_1920.jpg" },
-      { src: 'https://www.youtube.com/watch?v=uFXYwTafVqc?rel=0',
-        type: 'iframe' }
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is a default type
-  });
-  $('#meetro-popup').magnificPopup({
+  $('#Meetro').magnificPopup({
     items: [
       { src: 'img/Meetro/Meetro spread_1920x1080.jpg' },
       { src: 'img/Meetro/Peppermint Mocha_Meetro_main1080x1375.jpg' },
@@ -109,6 +88,44 @@ $(document).ready(function () {
       { src: 'img/Meetro/London Fog_Illustration_slide_1080.jpg' },
       { src: 'img/Meetro/Vanilla Latte Swirly web.jpg' },
       { src: 'img/Meetro/Meetro Pumpkin Latte_1080.jpg' }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+  });
+  $('#Logo').magnificPopup({
+    items: [
+      { src: 'img/logo/' }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+  });
+  $('#Tropito').magnificPopup({
+    items: [
+      { src: 'img/Farm2Fork/Farm 2 Fork Menu_menu.jpg' }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+  });
+  $('#Brand').magnificPopup({
+    items: [
+      { src: 'img/brand/JC main_1920x1080.jpg' },
+      { src: 'img/brand/Meetro spread_1920x1080.jpg' }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+  });
+  $('#Signage').magnificPopup({
+    items: [
+      { src: 'img/signage/JC main_1920x1080.jpg' },
+      { src: 'img/signage/Meetro spread_1920x1080.jpg' }
     ],
     gallery: {
       enabled: true
